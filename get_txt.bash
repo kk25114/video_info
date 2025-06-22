4336e95f5ea41a7a8040bf977458d7a277cfa0cf
312090307@qq.com <312090307@qq.com>
2025-06-22 10:52:56 +0800

    更新文稿 2025-06-22 10:52:56

--- get_txt.bash
+++ get_txt.bash
@@ -5,7 +5,6 @@ python3 get_transcripts.py "https://www.youtube.com/@yuegemovie" --output_dir "3
 python3 get_transcripts.py "https://www.youtube.com/@gleekid/videos" --output_dir "4.吟游诗人基德" --auto-commit --summarize --correct
 python3 get_transcripts.py "https://www.youtube.com/@voice-of-science/videos" --output_dir "5.科学声音" --auto-commit --summarize --correct
 python3 get_transcripts.py "https://www.youtube.com/@TianCaiJianShi/videos" --output_dir "6.天才简史" --auto-commit --summarize --correct
-python3 get_transcripts.py "https://www.youtube.com/@meanderingthoughts2927/videos" --output_dir "7.思维漫游" --auto-commit --summarize --correct
 python3 get_transcripts.py "https://www.youtube.com/@%E7%9F%A5%E8%AF%86%E6%9E%A2%E7%BA%BD/videos" --output_dir "8.知识枢纽" --auto-commit --summarize --correct
 python3 get_transcripts.py "https://www.youtube.com/@xiaoboreading/videos" --output_dir "9.小播讲哲学" --auto-commit --summarize --correct
 python3 get_transcripts.py "https://www.youtube.com/@dailyreading/videos" --output_dir "10.天天读书" --auto-commit --summarize --correct
