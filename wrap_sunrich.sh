b92186bd4898912d242bc624bb5a972f45cb7328
312090307@qq.com <312090307@qq.com>
2025-09-26 18:08:53 +0800

    重构0134文章格式并更新脚本

--- wrap_sunrich.sh
+++ wrap_sunrich.sh
@@ -30,8 +30,8 @@
 set -e
 
 # ---- 代理配置 ----
-export HTTP_PROXY="http://172.23.240.1:10806"
-export HTTPS_PROXY="http://172.23.240.1:10806"
+# export HTTP_PROXY="http://172.23.240.1:10806"
+# export HTTPS_PROXY="http://172.23.240.1:10806"
 export NO_PROXY="localhost,127.0.0.1,::1"
 
 cd /home/github/video_info
