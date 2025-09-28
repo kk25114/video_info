2f29d9f3f7068bf8e8267483d134d4d34227f608
312090307@qq.com <312090307@qq.com>
2025-09-28 12:37:22 +0800

    优化天文数字文章内容并更新发布脚本

--- publish_latest.sh
+++ publish_latest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-VIDEO_DIR="/home/github/video_info/mk_video"
+VIDEO_DIR="/mnt/d/Program Files/下载"
 LATEST_VIDEO=$(find "$VIDEO_DIR" -type f -name '*.mp4' -printf '%T@ %p\n' | sort -nr | head -n1 | cut -d' ' -f2-)
 
 if [[ -z "${LATEST_VIDEO:-}" ]]; then
